268066745a3caa07f7c8c22fdb3a9c95ee34e154
Jixin Han <kalfazed@gmail.com>
2024-08-10 07:39:54 +0900

    added poetry_test

---
+++ poetry_test/poetry_test/version.py
@@ -0,0 +1,4 @@
+version = "0.1.0"
+
+def print_info():
+    print(f"Version: {version}")
---
+++ poetry_test/tests/version_test.py
@@ -0,0 +1,7 @@
+from poetry_test.version import print_info
+
+def main():
+    print_info()
+
+if __name__ == "__main__":
+    main()
